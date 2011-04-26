46d15a4a6d2ae67d8eabe807ef7b17d951cbb49e
Tomasz Wysocki <tomasz@wysocki.info>
2011-04-26 15:27:17 +0200

    Fix FacebookJavaScriptSDKPackage.INSTALL_APPS.
    Change-Id: I068a6c6f8f3ca44f6f41ed0a11037c73aab8241d

--- facebook_javascript_sdk/__init__.py
+++ facebook_javascript_sdk/__init__.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, include
 from package_installer import Package
 
 class FacebookJavaScriptSDKPackage(Package):
-    INSTALL_APPS = ('facebook_javascript_sdk')
+    INSTALL_APPS = ('facebook_javascript_sdk',)
 
 PACKAGE = FacebookJavaScriptSDKPackage()
