64b728a432b58518585152c718e928dd67d0430a
Tomasz Wysocki <tomasz@wysocki.info>
2011-04-26 19:29:24 +0200

    Change package name.
    Change-Id: Ie153ebe00642abaecd68188646fe9e612356b1f4

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from distutils.core import setup
 
 setup(
-    name='FacebookJavaScriptSDK',
-    version='0.1',
+    name='facebook-javascript-sdk',
+    version='0.2',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
