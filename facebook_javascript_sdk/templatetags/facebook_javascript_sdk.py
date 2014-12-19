287812fa54006f66eb29dd078ac872be23d17cc6
Piotr Maliński <piotr@pozytywnie.pl>
2014-12-19 12:43:22 +0100

    Allow setting custom Facebook API version.
    Change-Id: I3c3c81c1732cf0bc62ec9bc4ceb1a06579f6d097

--- facebook_javascript_sdk/templatetags/facebook_javascript_sdk.py
+++ facebook_javascript_sdk/templatetags/facebook_javascript_sdk.py
@@ -3,17 +3,20 @@ from django import template
 
 register = template.Library()
 
+
 class FbInitNode(template.Node):
-    def __init__(self, script, app_id, *args, **kwargs):
+    def __init__(self, script, app_id, api_version, *args, **kwargs):
         super(FbInitNode, self).__init__(*args, **kwargs)
         self.script, self.app_id = script, app_id
+        self.api_version = api_version
+
     def render(self, context):
         language_code = context.get('FACEBOOK_LANGUAGE_CODE', 'pl_PL')
         return """
         <div id="fb-root"></div>
         <script type="text/javascript">
           window.fbAsyncInit = function() {
-            FB.init({version:'v2.1', appId: '%(id)s', status: true, cookie: true, xfbml: true});
+            FB.init({version:'%(version)s', appId: '%(id)s', status: true, cookie: true, xfbml: true});
             (function() {
                 %(script)s
             })();
@@ -25,7 +28,9 @@ class FbInitNode(template.Node):
             document.getElementById('fb-root').appendChild(e);
           }());
         </script>
-        """ % dict(id=self.app_id, script=self.script.render(context), lang=language_code)
+        """ % dict(version=self.api_version, id=self.app_id,
+                   script=self.script.render(context), lang=language_code)
+
 
 @register.tag
 def fb_init_block(parser, token, settings=project_settings):
@@ -40,4 +45,6 @@ def fb_init_block(parser, token, settings=project_settings):
     """
     script = parser.parse(('endblock',))
     parser.delete_first_token()
-    return FbInitNode(script, app_id=settings.FACEBOOK_APP_ID)
+    version = 'v%s' % getattr(settings, 'FACEBOOK_API_VERSION', '2.1')
+    return FbInitNode(script, app_id=settings.FACEBOOK_APP_ID,
+                      api_version=version)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='facebook-javascript-sdk',
-    version='1.2',
+    version='1.3',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
